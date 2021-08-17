fa4b5d8f2abce6d6efa3ce40f1a93a9df677c6a5
Rodolfo Olivieri <rodolfo.olivieri3@gmail.com>
2021-08-17 20:40:24 -0300

    updated project

--- src/main.rs
+++ src/main.rs
@@ -1,31 +1,50 @@
-use std::time::{Duration, Instant};
+use std::fs::File;
+use std::fs::OpenOptions;
+use std::io::prelude::*;
+use std::path::Path;
+use std::time::Instant;
+
+fn print_type_of<T>(_: &T) {
+    println!("{}", std::any::type_name::<T>())
+}
 
 fn save_results(seed: usize, previous_results: Vec<usize>) {
-    let mut file = match File::create("samples/{seed}")
+    let path = &format!("samples/{}.txt", seed);
+    let path = Path::new(path);
 
+    // Open a file in write-only mode, returns `io::Result<File>`
+    File::create(path).expect("Couldn't create file");
+    let mut file = OpenOptions::new()
+        .write(true)
+        .append(true)
+        .open(path)
+        .unwrap();
+    
+    for i in &previous_results {
+        writeln!(file, "{:?}", &i.to_le_bytes()[0]);
+    }
 }
 
 fn main() {
     std::fs::create_dir_all("samples/");
-    let mut seed = 0;
-    let mut previous_results: Vec<usize> = Vec::new();
+    let mut seed = 1;
     loop {
         let start = Instant::now();
         let mut value = seed;
-        previous_results.clear();
+        let mut previous_results: Vec<usize> = Vec::new();
         loop {
             value = (value & 1 == 1)
                 .then(|| (value * 3) + 1)
                 .unwrap_or(value / 2);
 
             previous_results.push(value);
-
+            // Immediatly break if we hit the number 1, as we know, this is a dead lock
             if value == 1 {
                 break;
             }
         }
-        save_results(seed, previous_results);
         println!("Seed {:?} took {:?}", seed, start.elapsed());
+        save_results(seed, previous_results);
         seed += 1;
     }
 }
